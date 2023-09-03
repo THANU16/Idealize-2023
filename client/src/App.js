bfc3c99b56713fd984673c05dc1203a865d79645
Lithurshan K <lithurshan2000@gmail.com>
2023-09-03 23:10:29 +0530

    hospital signup owner details page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import React from 'react';
 import Login from './Pages/login';
+import OwnerDetailsPage from './Pages/hospital/Signup/OwnerDetailsPage';
+import { BrowserRouter ,Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Login></Login>
+      {/* code to route login and signup */}
+      <OwnerDetailsPage></OwnerDetailsPage>
+
     </div>
   );
 }
