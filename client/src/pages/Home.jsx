7920954be589cdaf8d773bbd09a0349a5e3aef6f
TJSangaran <thevarasasangaran@gmail.com>
2023-09-04 23:14:27 +0530

    Fixed error in map paje

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,49 +4,11 @@ import {
   InfoWindow,
   Marker,
   useLoadScript,
-  DirectionsRenderer,
-  Circle,
-  MarkerClusterer,
 } from "@react-google-maps/api";
-
-import { useState, useMemo, useCallback, useRef } from "react";
+import { useState } from "react";
 import "./map.css";
-import Places from "../components/places";
-import Distance from "../components/distance";
 
 const Map = () => {
-  const [office, setOffice] = useState();
-  const [directions, setDirections] = useState();
-  const center = useMemo(() => ({ lat: 43.45, lng: -80.49 }), []);
-  const options = useMemo(
-    () => ({
-      // mapId: "b181cac70f27f5e6",
-      disableDefaultUI: true,
-      clickableIcons: false,
-    }),
-    []
-  );
-  const onLoad = useCallback((map) => (mapRef.current = map), []);
-  const houses = useMemo(() => generateHouses(center), [center]);
-
-  const fetchDirections = (house) => {
-    if (!office) return;
-
-    const service = new google.maps.DirectionsService();
-    service.route(
-      {
-        origin: house,
-        destination: office,
-        travelMode: google.maps.TravelMode.DRIVING,
-      },
-      (result, status) => {
-        if (status === "OK" && result) {
-          setDirections(result);
-        }
-      }
-    );
-  };
-
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
   });
@@ -56,7 +18,7 @@ const Map = () => {
   const markers = [
     { lat: 9.6702, lng: 80.0395 },
     { lat: 9.6652, lng: 80.0208 },
-    { lat: 9.6615, lng: 80.0255 },
+    { lat: 9.6615, lng: 73.7769 },
   ];
 
   const onMapLoad = (map) => {
@@ -73,8 +35,8 @@ const Map = () => {
   };
 
   return (
-    <div className="container">
-      <div className="map">
+    <div>
+      <div className="Map">
         {!isLoaded ? (
           <h1>Loading...</h1>
         ) : (
@@ -105,18 +67,7 @@ const Map = () => {
           </GoogleMap>
         )}
       </div>
-      <div className="controls">
-        {" "}
-        <h1>Commute?</h1>
-        <Places
-          setOffice={(position) => {
-            setOffice(position);
-            mapRef.current?.panTo(position);
-          }}
-        />
-        {!office && <p>Enter the address of your office.</p>}
-        {directions && <Distance leg={directions.routes[0].legs[0]} />}
-      </div>
+      <div>Hello</div>
     </div>
   );
 };
