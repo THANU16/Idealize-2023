0eee1ebcff2760c5a943c4b6e04f96190ca8dbbc
TJSangaran <thevarasasangaran@gmail.com>
2023-09-04 23:10:43 +0530

    Add map in home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,79 +1,124 @@
-import "./styles.css";
-import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import MarkerClusterGroup from "react-leaflet-cluster";
+/* global google */
+import {
+  GoogleMap,
+  InfoWindow,
+  Marker,
+  useLoadScript,
+  DirectionsRenderer,
+  Circle,
+  MarkerClusterer,
+} from "@react-google-maps/api";
 
-import { Icon, divIcon, point } from "leaflet";
+import { useState, useMemo, useCallback, useRef } from "react";
+import "./map.css";
+import Places from "../components/places";
+import Distance from "../components/distance";
 
-// create custom icon
-const customIcon = new Icon({
-  // iconUrl: "https://cdn-icons-png.flaticon.com/512/447/447031.png",
-  iconUrl: require("../assets/icons/placeholder.png"),
-  iconSize: [38, 38], // size of the icon
-});
+const Map = () => {
+  const [office, setOffice] = useState();
+  const [directions, setDirections] = useState();
+  const center = useMemo(() => ({ lat: 43.45, lng: -80.49 }), []);
+  const options = useMemo(
+    () => ({
+      // mapId: "b181cac70f27f5e6",
+      disableDefaultUI: true,
+      clickableIcons: false,
+    }),
+    []
+  );
+  const onLoad = useCallback((map) => (mapRef.current = map), []);
+  const houses = useMemo(() => generateHouses(center), [center]);
 
-// custom cluster icon
-const createClusterCustomIcon = function (cluster) {
-  return new divIcon({
-    html: `<span class="cluster-icon">${cluster.getChildCount()}</span>`,
-    className: "custom-marker-cluster",
-    iconSize: point(33, 33, true),
+  const fetchDirections = (house) => {
+    if (!office) return;
+
+    const service = new google.maps.DirectionsService();
+    service.route(
+      {
+        origin: house,
+        destination: office,
+        travelMode: google.maps.TravelMode.DRIVING,
+      },
+      (result, status) => {
+        if (status === "OK" && result) {
+          setDirections(result);
+        }
+      }
+    );
+  };
+
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
   });
+  const [mapRef, setMapRef] = useState();
+  const [isOpen, setIsOpen] = useState(false);
+  const [infoWindowData, setInfoWindowData] = useState();
+  const markers = [
+    { lat: 9.6702, lng: 80.0395 },
+    { lat: 9.6652, lng: 80.0208 },
+    { lat: 9.6615, lng: 80.0255 },
+  ];
+
+  const onMapLoad = (map) => {
+    setMapRef(map);
+    const bounds = new google.maps.LatLngBounds();
+    markers?.forEach(({ lat, lng }) => bounds.extend({ lat, lng }));
+    map.fitBounds(bounds);
+  };
+
+  const handleMarkerClick = (id, lat, lng, address) => {
+    mapRef?.panTo({ lat, lng });
+    setInfoWindowData({ id, address });
+    setIsOpen(true);
+  };
+
+  return (
+    <div className="container">
+      <div className="map">
+        {!isLoaded ? (
+          <h1>Loading...</h1>
+        ) : (
+          <GoogleMap
+            mapContainerClassName="map-container"
+            onLoad={onMapLoad}
+            onClick={() => setIsOpen(false)}
+          >
+            {markers.map(({ address, lat, lng }, ind) => (
+              <Marker
+                key={ind}
+                position={{ lat, lng }}
+                onClick={() => {
+                  handleMarkerClick(ind, lat, lng, address);
+                }}
+              >
+                {isOpen && infoWindowData?.id === ind && (
+                  <InfoWindow
+                    onCloseClick={() => {
+                      setIsOpen(false);
+                    }}
+                  >
+                    <h3>{infoWindowData.address}</h3>
+                  </InfoWindow>
+                )}
+              </Marker>
+            ))}
+          </GoogleMap>
+        )}
+      </div>
+      <div className="controls">
+        {" "}
+        <h1>Commute?</h1>
+        <Places
+          setOffice={(position) => {
+            setOffice(position);
+            mapRef.current?.panTo(position);
+          }}
+        />
+        {!office && <p>Enter the address of your office.</p>}
+        {directions && <Distance leg={directions.routes[0].legs[0]} />}
+      </div>
+    </div>
+  );
 };
 
-// markers
-const markers = [
-  {
-    geocode: [6.9271, 79.8612],
-    popUp: "Hello, I am pop up 1",
-  },
-];
-
-export default function App() {
-  return (
-    <MapContainer center={[6.9271, 79.8612]} zoom={13}>
-      {/* OPEN STREEN MAPS TILES */}
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {/* WATERCOLOR CUSTOM TILES */}
-      <TileLayer
-        attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg"
-      />
-      {/* GOOGLE MAPS TILES */}
-      <TileLayer
-        attribution="Google Maps"
-        url="http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}" // regular
-        // url="http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}" // satellite
-        // url="http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}" // terrain
-        maxZoom={20}
-        subdomains={["mt0", "mt1", "mt2", "mt3"]}
-      />
-
-      <MarkerClusterGroup
-        chunkedLoading
-        iconCreateFunction={createClusterCustomIcon}
-      >
-        {/* Mapping through the markers */}
-        {markers.map((marker) => (
-          <Marker position={marker.geocode} icon={customIcon}>
-            <Popup>{marker.popUp}</Popup>
-          </Marker>
-        ))}
-
-        {/* Hard coded markers */}
-        <Marker position={[51.505, -0.09]} icon={customIcon}>
-          <Popup>This is popup 1</Popup>
-        </Marker>
-        <Marker position={[51.504, -0.1]} icon={customIcon}>
-          <Popup>This is popup 2</Popup>
-        </Marker>
-        <Marker position={[51.5, -0.09]} icon={customIcon}>
-          <Popup>This is popup 3</Popup>
-        </Marker>
-      </MarkerClusterGroup>
-    </MapContainer>
-  );
-}
+export default Map;
