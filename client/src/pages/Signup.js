dfc179cd9760ddd2b786e77a20fe9a02a845c47e
Lithurshan K <lithurshan2000@gmail.com>
2023-09-05 21:22:50 +0530

    Signup caregory updates

---
+++ client/src/pages/Signup.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import HospitalIcon from "../images/Hospital.svg";
+import PeopleIcon from "../images/People.svg";
+import PharmacyIcon from "../images/Pharmacy.svg";
+import "./Signup.css";
+
+const Signup = () => {
+  return (
+    <div className="image">
+      <div className="image-container">
+        <h1>Categories...</h1>
+        <div className="triangle">
+          <Link to="/HospitalDetails">
+            <img src={HospitalIcon} alt="Image 1" className="category" />
+          </Link>
+          <Link to="/">
+            <img src={PeopleIcon} alt="Image 2" className="category"/>
+          </Link>
+          <Link to="/">
+            <img src={PharmacyIcon} alt="Image 3" className="category"/>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
