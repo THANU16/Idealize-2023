11d2152715b6506b5ec9361c0b4b23164d2af6cf
Thanu16 <kanagarajahthanushanth@gmail.com>
2023-09-14 19:25:46 +0530

    update initilize page

--- server/utils/databaseUtils.js
+++ server/utils/databaseUtils.js
@@ -8,12 +8,21 @@ class database {
     // this.password = "root";
     // this.database = "lifeserver";
 
-    this.host = "sql107.infinityfree.com";
+    // this.host = "sql107.infinityfree.com";
+    // // this.host = "192.168.219.247";
+    // this.port = 3306; // Replace with your desired port
+    // this.user = "if0_35023243";
+    // this.password = "wmQYLUnlxQ7kaf";
+    // this.database = "if0_35023243_lifeserver";
+
+    this.host = "familytree.mysql.database.azure.com";
     // this.host = "192.168.219.247";
     this.port = 3306; // Replace with your desired port
-    this.user = "if0_35023243";
-    this.password = "wmQYLUnlxQ7kaf";
-    this.database = "if0_35023243_lifeserver";
+    this.user = "kobinarth22";
+    this.password = "Itsme043";
+    this.database = "lifeserver";
+
+
 
     this.connection = mysql.createConnection({
       host: this.host,
