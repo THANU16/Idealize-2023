8bfcb20b6541e1f44df6cf67fd419736aca254b9
Thevarasa Sangaran <93479754+TJSangaran@users.noreply.github.com>
2023-09-26 15:41:17 +0530

    Update SignupFinished.js

--- client/src/pages/Hospital/Signup/SignupFinished.js
+++ client/src/pages/Hospital/Signup/SignupFinished.js
@@ -8,14 +8,9 @@ function SignupFinished() {
     <div className="upload">
       <div className="file-upload-container">
         <h3>Documents have been submitted successfully!</h3>
-        <p>
-          We will send the username and password to your email after
-          verification.
-        </p>
+        <p>We will send the username and password to your email after verification.</p>
         <p>Thank you.</p>
-        <button type="button" className="signup_finished_btn">
-          <NavLink to="/login"> Go to Login Page</NavLink>
-        </button>
+        <button type='button'><NavLink style={{color:'white'}} to='/login'> Go to Login Page</NavLink></button>
       </div>
     </div>
   );
