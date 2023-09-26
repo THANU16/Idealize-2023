8fd2e1cbffd6c607406eb69fcf7987a87f79970c
Nithushi <nithursi2000@gmail.com>
2023-09-26 22:12:16 +0530

    driver page

---
+++ client/src/pages/Ambulance/Home.jsx
@@ -0,0 +1,223 @@
+import React, { useState, useEffect } from "react";
+import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
+import ambulanceMarkerIcon from "../../assets/icons/map_ambulance.svg";
+import deiverMarkerIcon from "../../assets/icons/placeholder.png";
+import "../styles.css";
+
+import Table from "react-bootstrap/Table";
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng,
+} from "react-places-autocomplete";
+
+import axios from "axios";
+
+const Home = (props) => {
+  // const { onRequest, onCancel } = props;
+  const [selectedPlace, setSelectedPlace] = useState(null);
+  const [coordinates, setCoordinates] = useState(null);
+  const [address, setAddress] = useState("");
+  const [ambulanceLocation, setAmbulanceLocation] = useState({});
+  const [requestData, setRequestData] = useState({});
+
+  const [userLocation, setUserLocation] = useState({
+    latitude: null,
+    longitude: null,
+  });
+  // Store driver's location here
+  useEffect(() => {
+    const sessionToken = JSON.parse(sessionStorage.getItem("sessionToken"));
+
+    axios
+      .post(
+        `${process.env.REACT_APP_API_URL}/hospital/getHospitalAmbulanceLocation`,
+        {},
+        { headers: { Authorization: "key " + sessionToken } }
+      )
+      .then((res) => {
+        if (res.data.sucess) {
+          setAmbulanceLocation(res.data.results);
+        }
+      })
+      .catch((err) => console.log(err));
+
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/hospital/getRequest`)
+      .then((res) => {
+        if (res.data.sucess) {
+          setRequestData(res.data.results);
+        }
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const onMapClick = (mapProps, map, event) => {
+    const clickedLatitude = event.latLng.lat();
+    const clickedLongitude = event.latLng.lng();
+
+    setSelectedPlace(null);
+    setCoordinates({ latitude: clickedLatitude, longitude: clickedLongitude });
+  };
+
+  const onMarkerClick = (props, marker, e) => {
+    setSelectedPlace(props);
+    setCoordinates(null);
+  };
+
+  const handleSelect = async (address) => {
+    try {
+      const results = await geocodeByAddress(address);
+      const latLng = await getLatLng(results[0]);
+
+      setSelectedPlace(results[0]);
+      setCoordinates({ latitude: latLng.lat, longitude: latLng.lng });
+      setAddress(address);
+    } catch (error) {
+      console.error("Error selecting location:", error);
+    }
+  };
+
+  const sendLocationDataToBackend = (lat, lng) => {
+    // Send the coordinates to the backend using an Axios POST request
+    // You can use Axios or any other method to send the data
+    // Example:
+    // axios.post('/api/saveLocation', { latitude: lat, longitude: lng })
+    //   .then((response) => {
+    //     console.log(response.data);
+    //   })
+    //   .catch((error) => {
+    //     console.error('Error sending location data:', error);
+    //   });
+  };
+  const [request, setRequest] = useState(false);
+  const onCancel = () => {
+    setRequest(false);
+  };
+
+  const onRequest = () => {
+    setRequest(false);
+  };
+  const locations = [
+    {
+      lat: 9.6638,
+      lng: 80.0208, // Jaffna Town
+      bearing: 0, // Bearing is set to 0 for reference
+    },
+
+  ];
+
+  const [showNotifications, setShowNotifications] = useState(false);
+
+  // Function to toggle the visibility of notifications
+  const toggleNotifications = () => {
+    setShowNotifications(!showNotifications);
+  };
+  useEffect(() => {
+    // Check if geolocation is available in the user's browser
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+
+        // Set the user's location in the state
+        setUserLocation({ latitude, longitude });
+      });
+    } else {
+      console.log("Geolocation is not available in this browser.");
+    }
+  }, []);
+
+  // Show the emergency request modal if request is true
+  if (request)
+    return (
+      <div className="emergency-request-modal">
+        <div className="emergency-request-content">
+          <div className="emergency-header">
+            <h1>
+              There is an emergency{" "}
+              <div className="emergency-center">
+                <img
+                  src="https://media.istockphoto.com/photos/emergency-symbol-picture-id453100595?k=6&m=453100595&s=170667a&w=0&h=Bi6sk8KHGJLcqZ5awSX7_i0esgjsWTMIdVn_EOaS2xo="
+                  alt="Emergency"
+                  width="100"
+                  height="100"
+                />
+              </div>
+            </h1>{" "}
+            {/* Add the ambulance emoji */}
+          </div>
+          <p>
+            <h2>Please accept request and send the ambulance</h2>
+          </p>
+          <div className="emergency-button-container">
+            <button className="reject-button" onClick={onCancel}>
+              Reject
+            </button>
+            <button className="accept-button" onClick={onRequest}>
+              Accept
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+
+  // Show the map if request is false
+  return (
+    <div className="container">
+      <div className="map">
+        {/* Render the Google Map */}
+        {userLocation.latitude && userLocation.longitude && (
+        <Map
+         
+          google={props.google}
+          zoom={14}
+          initialCenter={{ lat: userLocation.latitude,lng: userLocation.longitude, }}
+          mapContainerClassName="map-container"
+        >
+          {/* Map each location to a Marker */}
+          {locations.map((location, index) => (
+            <Marker
+              key={index}
+              position={{ lat: location.lat, lng: location.lng }}
+              icon={{
+                url: ambulanceMarkerIcon,
+                scaledSize: new window.google.maps.Size(100, 100),
+              }}
+            />
+          ))}
+          {/* Add a Marker for the user's location */}
+          
+            <Marker
+              position={{
+                lat: userLocation.latitude,
+                lng: userLocation.longitude,
+              }}
+              icon={{
+                url: deiverMarkerIcon,
+                scaledSize: new window.google.maps.Size(100, 100),
+              }}
+            />
+          
+            </Map>)}
+        
+      </div>
+      {/* <div>
+        <h1>User's Current Location:</h1>
+        {userLocation.latitude && userLocation.longitude ? (
+          <div>
+            <p>Latitude: {userLocation.latitude}</p>
+            <p>Longitude: {userLocation.longitude}</p>
+          </div>
+        ) : (
+          <p>Fetching location...</p>
+        )}
+      </div> */}
+      {/*Active ambulance details */}
+      
+    </div>
+  );
+};
+
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyAl5YvfOlFxEH09-MkWNh9OhYoQdN3uJOs", // Replace with your API key
+})(Home);
