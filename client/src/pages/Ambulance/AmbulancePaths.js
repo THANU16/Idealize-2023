1f32f75557ea63a2d0c4b3493d63f72dbb5d1a16
Pairavi <pairavi.20@cse.mrt.ac.lk>
2023-09-27 17:01:39 +0530

    driver direction

--- client/src/pages/Ambulance/AmbulancePaths.js
+++ client/src/pages/Ambulance/AmbulancePaths.js
@@ -5,6 +5,8 @@ import Home from "./Home";
 import Help from "./Pages/Help.jsx";
 import Logut from "./Pages/Logout.jsx";
 import Sidebar from "./SideBar/Sidebar";
+import Direction from "./Pages/Direction";
+
 // import Ambulance from "./Ambulance/Ambulance";
 
 function AmbulancePaths() {
@@ -13,21 +15,9 @@ function AmbulancePaths() {
     <Sidebar path="/">
       <Routes>
         <Route path="/home" element={<Home />} />
-        {/* <Route path="/ambulance" element={<Ambulance />} />
-        <Route path="/drivers" element={<Drivers />} /> */}
         <Route path="/help" element={<Help />} />
         <Route path="/logout" element={<Logut />} />
-        {/* signup */}
-        {/* <Route path="/SignupFinished" element={<SignupFinished />} />
-        <Route path="/HospitalDetails" element={<HospitalDetails />} />
-        <Route path="/OwnerDetails" element={<OwnerDetails />} /> */}
-        {/* <Route
-          path="/UploadHospitalDocuments"
-          element={<UploadHospitalDocuments />}
-        /> */}
-        {/* <Route path="/HospitalSearch" element={<HospitalSearch />} /> */}
-        {/* <Route path="/ambulanceForm" element={<AmbulanceForm />} />
-        <Route path="/driverForm" element={<DriverForm />} />/ */}
+        <Route path="/show" element={<Direction />} />
       </Routes>
     </Sidebar>
   );
