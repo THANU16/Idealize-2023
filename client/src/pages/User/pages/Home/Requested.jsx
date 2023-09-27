ddeeb1c3794ac130de1172ffc653d0cab8636663
TJSangaran <thevarasasangaran@gmail.com>
2023-09-27 21:05:56 +0530

    user home page updated

--- client/src/pages/User/pages/Home/Requested.jsx
+++ client/src/pages/User/pages/Home/Requested.jsx
@@ -1,35 +1,64 @@
+// import React, { useEffect, useState } from "react";
+// import "./Requested.css";
+// import MydModalWithGrid from "../../../../components/MydModalWithGrid";
+// import Button from "react-bootstrap/Button";
+
+// function App() {
+//   const [isToggleBarOpen, setToggleBarOpen] = useState(false);
+
+//   const activateToggleBar = () => {
+//     setToggleBarOpen(!isToggleBarOpen);
+//     console.log(isToggleBarOpen);
+//   };
+
+//   // useEffect(() => {
+//   //   console.log(isToggleBarOpen);
+//   // }, [isToggleBarOpen]);
+//   const [modalShow, setModalShow] = useState(false);
+
+//   return (
+//     <div>
+//       <div
+//         style={{
+//           display: "flex",
+//           justifyContent: "center",
+//           alignItems: "center",
+//         }}
+//       >
+//         <Button
+//           style={{ backgroundColor: "purple" }}
+//           onClick={() => setModalShow(true)}
+//         >
+//           Launch modal with grid
+//         </Button>
+//       </div>
+
+//       <MydModalWithGrid show={modalShow} onHide={() => setModalShow(false)} />
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import emgbtn from "../../usericons/emergency.png";
-// import "../user.css";
+import "../user.css";
 import { NavLink } from "react-router-dom";
 import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
 import pending from "../../usericons/pending.png";
 import cancel from "../../usericons/cancel.png";
 import axios from "axios";
 import "./Requested.css";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Modal from "react-bootstrap/Modal";
+import Row from "react-bootstrap/Row";
 
 const RequestCancel = (props) => {
-  const requestData = JSON.parse(sessionStorage.getItem("requestData"));
+  const [currentLocation, setCurrentLocation] = useState(null);
   const navigate = useNavigate();
-  const currentLocation = { lat: requestData[0].lat, lng: requestData[0].lng };
-
-  useEffect(() => {
-    console.log(currentLocation);
-    axios
-      .post(`${process.env.REACT_APP_API_URL}/hospital/getRecentRequest`, {
-        data: requestData[0].requestID,
-      })
-      .then((res) => {
-        console.log(res.data);
-        if (res.data.sucess) {
-          if (res.data.result.status == "Accepted") {
-            navigate("/show");
-          }
-        }
-      })
-      .catch((err) => console.log(err));
-  });
 
   const handleCancelRequest = () => {
     navigate("/request/cancel");
@@ -63,6 +92,7 @@ const RequestCancel = (props) => {
         </Map>
       </div>
       {/*Active ambulance details */}
+      <div></div>
     </div>
   );
 };
@@ -70,49 +100,3 @@ const RequestCancel = (props) => {
 export default GoogleApiWrapper({
   apiKey: "AIzaSyAl5YvfOlFxEH09-MkWNh9OhYoQdN3uJOs", // Replace with your API key
 })(RequestCancel);
-
-// <div className="controls">
-//         <div className="tables">
-//           <h3>
-//             Request Status
-//             <div>
-//               <span>
-//                 <img src={pending} />
-//               </span>
-//             </div>
-//           </h3>
-//           <table className="table table-bordered table-striped table-hover ">
-//             <thead>
-//               <tr>
-//                 <th width="20px">Req.rec.By</th>
-//                 <th width="20px">Count</th>
-//               </tr>
-//             </thead>
-//             <tbody>
-//               {/* {transactionData.map((data) => ( */}
-//               <tr>
-//                 <td>Ambulances</td>
-//                 <td>5</td>
-//               </tr>
-//               <tr>
-//                 <td>Hospitals</td>
-//                 <td>4</td>
-//               </tr>
-//             </tbody>
-//           </table>
-//         </div>
-
-//         <div className="notifications">
-//           <div clasName="cancelRequest">
-//             <img
-//               src={cancel}
-//               style={{ width: "25vh" }}
-//               onClick={handleCancelRequest}
-//             />
-//           </div>
-
-//           {/* Render notifications based on the state */}
-
-//           {/* Render other components as needed */}
-//         </div>
-//       </div>
