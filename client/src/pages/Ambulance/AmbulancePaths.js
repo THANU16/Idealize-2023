64e44b2e4792a3ff870a5cb55c87039e8b77ded5
Nithushi <nithursi2000@gmail.com>
2023-09-28 18:23:56 +0530

    updated

--- client/src/pages/Ambulance/AmbulancePaths.js
+++ client/src/pages/Ambulance/AmbulancePaths.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./Home";
 // import Drivers from "./Driver/Drivers";
 import Help from "./Pages/Help.jsx";
-import Logut from "./Pages/Logout.jsx";
+import Logout from "./Pages/Logout.jsx";
 import Sidebar from "./SideBar/Sidebar";
 import Direction from "./Pages/Direction";
 import DriverProfile from "./Pages/DriverProfile";
@@ -18,7 +18,7 @@ function AmbulancePaths() {
         <Route path="/driverProfile" element={<DriverProfile/>}/>
         <Route path="/home" element={<Home />} />
         <Route path="/help" element={<Help />} />
-        <Route path="/logout" element={<Logut />} />
+        <Route path="/logout" element={<Logout />} />
         <Route path="/show" element={<Direction />} />
         <Route path="/homeAfter" element={<HomeAfter/>}/>
       </Routes>
