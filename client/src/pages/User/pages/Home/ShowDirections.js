83947b9d389a52301dc215d4cd3ea9bcc2be219a
Pairavi <pairavi.20@cse.mrt.ac.lk>
2023-09-29 14:00:29 +0530

    usershowpath changes

--- client/src/pages/User/pages/Home/ShowDirections.js
+++ client/src/pages/User/pages/Home/ShowDirections.js
@@ -1,39 +1,42 @@
 import React, { Component } from "react";
 import { GoogleApiWrapper } from "google-maps-react";
-import axios from "axios";
 
 class ShowPath extends Component {
   constructor(props) {
     super(props);
+    const acceptData = JSON.parse(sessionStorage.getItem("sessionToken"));
     this.state = {
-      //   origin: { lat: 12.9802347063322, lng: 77.5907760360903 }, // Replace with your origin latitude and longitude
-      //   destination: { lat: 13.9793774204024, lng: 78.5910979011596 }, // Replace with your destination latitude and longitude
-      origin: null,
+      // origin: { lat: 6.7880706, lng: 79.8912813 },
+      origin: { lat: acceptData.lat, lng: acceptData.longitude },
+      // destination: { lat: 13.9793774204024, lng: 78.5910979011596 },
       destination: null,
     };
   }
 
   componentDidMount() {
-    // Call the API to fetch origin and destination data
-    const sessionToken = JSON.parse(sessionStorage.getItem("sessionToken"));
-    axios
-      .get(`${process.env.REACT_APP_API_URL}/xxxxxxxxxxxxxxx`, {
-        headers: { Authorization: "key " + sessionToken },
-      })
-      .then((res) => {
-        if (res.data.success) {
-          this.setState({
-            origin: { lat: res.data.originLat, lng: res.data.originLng },
-            destination: {
-              lat: res.data.destinationLat,
-              lng: res.data.destinationLng,
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const latitude = position.coords.latitude;
+          const longitude = position.coords.longitude;
+          this.setState(
+            {
+              destination: { lat: latitude, lng: longitude },
             },
-          });
-          // Initialize the map once data is fetched
-          this.initMap();
+            () => {
+              // Initialize the map once destination is obtained
+              this.initMap();
+            }
+          );
+        },
+        (error) => {
+          console.error("Geolocation error:", error);
+          // Handle the error, e.g., by providing a user-friendly message
         }
-      })
-      .catch((err) => console.log(err));
+      );
+    } else {
+      console.error("Geolocation is not available in this browser.");
+    }
   }
 
   initMap() {
